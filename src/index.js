ce6d4187c87051f9bd1ee7763a1d63b432e8d44d
Artur Wojciechowski <awojciechowski@insolutions.pl>
2017-11-22 15:11:58 +0100

    feat(base): Adds basic functionality - createStore, compose, mutate and get and tests to them

--- src/index.js
+++ src/index.js
@@ -1,6 +1,47 @@
 import 'babel-polyfill'
 
-const compose = (...functions) => async value => {
+export function clone (item) {
+  if (!item) { return item }
+
+  let types = [ Number, String, Boolean ]
+
+  for (let type of types) {
+    if (item instanceof type) {
+      return type(item)
+    }
+  }
+
+  if (Array.isArray(item)) {
+    let result = []
+
+    for (let e of item) {
+      result.push(clone(e))
+    }
+
+    return result
+  }
+
+  if (item instanceof Date) {
+    return new Date(item)
+  }
+
+  if (item instanceof RegExp) {
+    return new RegExp(item)
+  }
+
+  if (typeof item === 'object') {
+    let result = {}
+    for (let [ key, value ] of Object.entries(item)) {
+      result[key] = clone(value)
+    }
+
+    return result
+  }
+
+  return item
+}
+
+export const compose = (...functions) => async value => {
   let result = value
 
   for (const fn of functions) {
@@ -10,6 +51,96 @@ const compose = (...functions) => async value => {
   return result
 }
 
-module.exports = {
-  compose
+export const mutate = ppath => (...pmutations) => async psubject => {
+  let path = await ppath
+  let paths = path.split('.')
+  let mutation = await compose(...pmutations)
+  let subject = await psubject
+
+  let root = clone(subject)
+  let target = root
+  let fields = paths.slice(0, -1)
+  let lastField = paths.slice(-1)
+
+  for (const field of fields) {
+    target = target[field]
+  }
+
+  target[lastField] = await mutation(target[lastField])
+  return root
+}
+
+export const get = ppath => psubject => async (...pmutation) => {
+  let path = await ppath
+  let paths = path.split('.')
+  let mutation = await compose(...pmutation)
+  let subject = await psubject
+
+  let target = clone(subject)
+  let fields = paths.slice(0, -1)
+  let lastField = paths.slice(-1)
+
+  for (const field of fields) {
+    target = target[field]
+  }
+
+  return mutation(target[lastField])
+}
+
+export const bind = fn => (...args) => (...rest) => fn(...args, ...rest)
+
+export function createStore (initialState) {
+  let state = initialState
+  let subscribers = []
+
+  function subscribe (handler) {
+    subscribers = subscribers.concat(handler)
+  }
+
+  function notify (state) {
+    subscribers.forEach(subscriber => {
+      subscriber(state)
+    })
+  }
+
+  function getState () {
+    return state
+  }
+
+  function isEqual (x, y) {
+    return equals(x, y, function compareInner (a, b) {
+      if (typeof a === 'object' && typeof b === 'object') {
+        return equals(a, b, compareInner)
+      }
+
+      return a === b
+    })
+  }
+
+  function dispatchSync (morphism) {
+    let newState = morphism(state)
+
+    if (!isEqual(state, newState)) {
+      state = newState
+
+      notify(state)
+    }
+  }
+
+  function dispatch (morphism) {
+    morphism(state).then(newState => {
+      if (!isEqual(state, newState)) {
+        state = newState
+
+        notify(state)
+      }
+    })
+  }
+
+  return {
+    subscribe,
+    getState,
+    dispatchSync,
+    dispatch
+  }
 }
--- test/index.js
+++ test/index.js
@@ -11,3 +11,34 @@ test('compose composes all functions', async t => {
 
   t.is(result, 45)
 })
+
+test('clone creates a deep clone of an object', t => {
+  t.plan(2)
+
+  const original = { a: { b: 42 }, c: new Date('13-12-2017') }
+  const copy = lib.clone(original)
+
+  original.a.b = 32
+
+  t.not(original, copy)
+  t.not(original.a.b, copy.a.b)
+})
+
+test('mutate changes a field deep inside the object using a mutation function', async t => {
+  const source = { counter: 10 }
+  const mutation = x => x / 2
+
+  const result = await lib.mutate('counter')(mutation)(source)
+
+  t.is(result.counter, 5)
+})
+
+test('createStore returns an object with the same interface as redux store', t => {
+  t.plan(3)
+
+  const store = lib.createStore()
+
+  t.true(typeof store.getState === 'function')
+  t.true(typeof store.dispatch === 'function')
+  t.true(typeof store.subscribe === 'function')
+})
